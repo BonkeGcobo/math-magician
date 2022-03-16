3df2328d073b9f96869ae1e20a3d5fd9f1ac58d7
BonkeG <bonkegcobo291195@gmail.com>
2022-03-16 18:51:15 +0200

    Change the App functional component to a class component component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <Calculator />
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
